b481bbdeb65bc1f86fafb17089dcbc24377d2c7f
Clive Nyaga <nyagakagotho@gmail.com>
2026-01-24 19:29:19 +0300

    Creates a movie searching form

--- react-movie-application/src/pages/Home.jsx
+++ react-movie-application/src/pages/Home.jsx
@@ -1,14 +1,27 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "Inception", release_date: "2010" },
         { id: 2, title: "The Matrix", release_date: "1999" },
         { id: 3, title: "Interstellar", release_date: "2014" }
     ];
 
+    const handleSearch = () => {};
+
     return (
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" 
+                placeholder="Search movies..."
+                className="search-input"
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
             <div className="movies-grid">
             {movies.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
