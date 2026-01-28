542fd9b67db6860cd03f1c10ae7ab247ddc4f6d0
Clive Nyaga <nyagakagotho@gmail.com>
2026-01-28 09:07:32 +0300

    Corrects styling of movie cards, wraps the application in a context, adds functions for managing favorite movies

--- react-movie-application/src/components/MovieCard.jsx
+++ react-movie-application/src/components/MovieCard.jsx
@@ -1,9 +1,18 @@
 import "../css/MovieCard.css";
+import { useMovieContext } from "../contexts/MovieContext";
 
 function MovieCard({movie}) {
+    const { addToFavorites, removeFromFavorites, isFavorite } = useMovieContext();
+    const favorite = isFavorite(movie.id);
 
-    function onFavoriteClick() {
-        alert("Favorite button clicked!");
+    function onFavoriteClick(e) {
+        e.preventDefault();
+        if (favorite) {
+            removeFromFavorites(movie.id);
+        } else {
+            addToFavorites(movie);
+        }
+        // alert("Favorite button clicked!");
     }
 
   return (
@@ -11,7 +20,7 @@ function MovieCard({movie}) {
       <div className="movie-poster">
         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
         <div className="movie-overlay">
-            <button className="favorite-btn" onClick={onFavoriteClick}>
+            <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
                 🤍
             </button>
         </div>
